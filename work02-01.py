a5b6ac0b1e701fc551a048e6bd03a309b7651d0e
MitsuyasuTanioka <mitsuyasu.tanioka@nds-osk.co.jp>
2025-05-16 09:41:30 +0900

    第2章演習問題提出

---
+++ work02-01.py
@@ -0,0 +1,17 @@
+# (1)
+a = 3
+b = 4
+
+# (2)
+print(a * b)
+
+# (3)
+print(str(a) + "×" + str(b) + "=" + str(a*b))
+
+# (4)
+s1 = "value: 123"
+print(s1[7:])
+
+# (5)
+s2 = "I like dog"
+print(s2.replace("dog", "cat"))
